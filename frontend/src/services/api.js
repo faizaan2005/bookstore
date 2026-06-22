f257f81f580238f988f894c01e3e96f0ef8f7d95
faizaan2005 <23eg107c38@anurag.edu.in>
2026-06-22 15:51:40 +0530

    Hardcode backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://bookstore-backend-krml.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
